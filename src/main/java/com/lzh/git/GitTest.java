4277e411a66ca8d6a291cce70f27784c234ca775
lzh <1036575272@qq.com>
2022-07-26 20:20:17 +0800

    second commit

--- src/main/java/com/lzh/git/GitTest.java
+++ src/main/java/com/lzh/git/GitTest.java
@@ -3,5 +3,6 @@ package com.lzh.git;
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git!");
+        System.out.println("hello git2!");
     }
 }
